d70fb385061b50d3113ca1f09c8ce8ebc14bd763
Adryan <adryan.luz.freitas@gmail.com>
2022-04-07 11:13:20 -0400

    :sparkles: Finishing beautiful App and Forms From Another Planet, starting JavaScript guide

---
+++ 07-Guia Estelar JavaScript/01-PrimeirosPassos/01-scripts copy.js
@@ -0,0 +1,10 @@
+// Aqui eu tenho um comentário em linha 
+
+/*
+    Aqui eu tenho um comentário multi linha
+
+    o código abaixo escreve alguma mensagem no devtools
+
+*/
+
+console.log('Bem vindos ao Starter')
