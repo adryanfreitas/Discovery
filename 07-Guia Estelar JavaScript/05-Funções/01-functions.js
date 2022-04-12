39a68711baabac58dee73751b3b759c79df644e6
Adryan <adryan.luz.freitas@gmail.com>
2022-04-12 10:59:44 -0400

    :sparkles: Studying Functions in JavaScript Star Guide

---
+++ 07-Guia Estelar JavaScript/05-Funções/01-functions.js
@@ -0,0 +1,23 @@
+// // function expression
+// // function anonymous
+// const sum = function(number1, number2){
+//     total = number1 + number2
+//     return total
+// }
+
+// let number1 = 34
+// let number2 = 25
+
+// // console.log(`o número 1 é ${number1}`)
+// // console.log(`o número 2 é ${number2}`)
+// console.log(`a soma é ${sum(number1, number2)}`)
+// console.log(total)
+
+function fazerSuco(fruta1, fruta2) {
+    return 'suco de: ' + fruta1 + fruta2
+}
+
+const copo = fazerSuco('banana', 'maçã')
+
+
+console.log(copo)
