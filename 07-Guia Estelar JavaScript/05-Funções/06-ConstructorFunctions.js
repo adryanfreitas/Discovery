5ff012564508850b5d331fa83baed76244486fe1
Adryan <adryan.luz.freitas@gmail.com>
2022-04-13 11:06:37 -0400

    :sparkles: Ending Functions and  Started Manipulating Data in JavaScript Guid Star

---
+++ 07-Guia Estelar JavaScript/05-Funções/06-ConstructorFunctions.js
@@ -0,0 +1,21 @@
+/*
+    Function() constructor
+
+    * expressão new
+    * criar um novo objeto
+    * this key word
+*/
+
+let date = new Date("2022-04-13")
+
+console.log(date)
+
+// function Person(name) {
+//     this.name = name
+// }
+
+
+// const mayk = new Person("Mayk")
+// const joao = new Person("Joao")
+// console.log(mayk)
+// console.log(joao)
