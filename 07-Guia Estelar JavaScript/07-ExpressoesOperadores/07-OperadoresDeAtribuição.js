b7718733e9d3df5d630e65ad79d4f17713f0c6e6
Adryan <adryan.luz.freitas@gmail.com>
2022-04-19 11:14:43 -0400

    :sparkles: Taking classes on Operator Expressions in the Star Guide JavaScript module

---
+++ 07-Guia Estelar JavaScript/07-ExpressoesOperadores/07-OperadoresDeAtribuição.js
@@ -0,0 +1,28 @@
+//  Operadores de atribuição (Assignment)
+let x
+
+console.log(x)
+
+// Assignment
+x = 1
+
+// addition Assignment
+// x = x + 2
+x += 2
+
+//  subtraction Assignment
+// x = x - 1
+x -= 2
+
+// multiplication Assignment
+// x = x * 2
+x *= 2
+
+// division Assignment
+// x = x / 2
+x /= 2
+
+// remainder, exponetiation
+// x%= 2
+x **= 2
+console.log(x)
