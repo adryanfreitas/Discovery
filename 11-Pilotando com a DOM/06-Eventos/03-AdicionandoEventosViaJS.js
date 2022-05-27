777a8706da0db002009bea10686944b1979de655
Adryan <adryan.luz.freitas@gmail.com>
2022-05-27 11:11:09 -0400

    :sparkles: Finishing Piloting with the DOM

---
+++ 11-Pilotando com a DOM/06-Eventos/03-AdicionandoEventosViaJS.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('click', print)
+
+function print() {
+    console.log('print')
+}
---
+++ 11-Pilotando com a DOM/06-Eventos/04-AdicionandoEventosViaJS(OutroMetodo).js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector('h1');
+
+h1.addEventListener = ('click', print)
+
+function print() {
+    console.log('print')
+}
+
+h1.addEventListener('click',function() {
+    console.log('outro momento')
+})
