6d616854fc074265b515a68f24cb79a364cac1c9
Adryan <adryan.luz.freitas@gmail.com>
2022-06-15 11:21:00 -0400

    :sparkles:  Starting NodeJs

---
+++ 12-NodeJs/01-IniciandoNaPratica/02-require.js
@@ -0,0 +1,7 @@
+// const path = requile('path')
+
+// console.log(path.basename(__filename))
+
+const myModule = require('./03-exports')
+
+console.log(myModule)
---
+++ 12-NodeJs/01-IniciandoNaPratica/05-greetings.js
@@ -0,0 +1,3 @@
+const getFlagsValue = require('./05-desafio')
+
+console.log(`Oi ${getFlagsValue('--name')}. ${getFlagsValue('--greeting')}}`)
