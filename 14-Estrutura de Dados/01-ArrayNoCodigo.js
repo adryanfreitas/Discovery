e8e0b00bdc54d9db255eebda85cfec769b0032b4
Adryan <adryan.luz.freitas@gmail.com>
2022-07-12 13:32:22 -0400

    feat: :sparkles: Making the Matrix class in the Data structure module

---
+++ 14-Estrutura de Dados/01-ArrayNoCodigo.js
@@ -0,0 +1,26 @@
+const pilotos = ['Senna', 'Prost', 'Schumacher', 'Hamilton']
+
+// a indexação (index) começa pelo número 0
+console.log(pilotos[0])
+console.log(pilotos[3])
+
+// acessar o tamanho do array
+console.log(pilotos.length)
+
+// iterável
+for (let piloto of pilotos) {
+    console.log(piloto)
+}
+
+// adicionar elemento 
+pilotos.push('Alonso')
+console.log(pilotos)
+
+// encontar um elemento 
+const senna = pilotos.find(piloto => piloto === 'Senna')
+console.log(senna)
+
+// deletar um elemento 
+pilotos.splice(1, 1)
+
+console.log(pilotos)
