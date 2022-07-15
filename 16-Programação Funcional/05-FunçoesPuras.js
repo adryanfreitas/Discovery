f82a4e60c59ac4df282a6544490b87d7f5f73dcc
Adryan <adryan.luz.freitas@gmail.com>
2022-07-15 08:48:11 -0400

    feat: :sparkles: Pure Functions in functional programming module

---
+++ 16-Programação Funcional/05-FunçoesPuras.js
@@ -0,0 +1,30 @@
+// Função impura
+
+// Exemplo 1: está dependendo de dado externo
+// que não foi passado como parâmetro
+function calculateCircumference(radius) {
+    return Math.PI * (radius + radius)
+}
+
+// Exemplo 2: está alterando um dado externo
+let person = {
+    name: 'Rafael Camarda',
+    age: 'jovem'
+}
+function changeName(name) {
+    person.name = name
+}
+
+// Função pura
+
+// Exemplo 1
+const calculateCircumference = function (pi, radius) {
+    return pi * (radius + radius)
+}
+
+// com arrow function
+const calculateCircumference = (pi, radius) =>
+    pi * (radius + radius)
+
+// Exemplo 2
+const changePersonName = (person, name) => ({...person, name})
