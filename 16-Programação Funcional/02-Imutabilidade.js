4ad4b14324bb46c165c7d6882e4afb4a1c66daa1
Adryan <adryan.luz.freitas@gmail.com>
2022-07-15 07:54:25 -0400

    feat: :sparkles: Immutability in functional programming module

---
+++ 16-Programação Funcional/02-Imutabilidade.js
@@ -0,0 +1,20 @@
+// Exemplo em JS
+const cart = {
+    quantity: 2,
+    total: 200
+}
+
+// bad
+cart.quantity = 3
+
+// good
+const newCart = {...cart, quantity: 3}
+
+// Exemplo em ReactJS
+const [amount, setAmount] = useState(0)
+
+// bad
+amount = 2
+
+// good
+setAmount(2)
