b4894e8c11d92c9e8f93d38566adf84a549405e7
Adryan da Luz Freitas <Adryan.luz.freitas@gmail.com>
2022-08-04 18:06:00 -0400

    feat: :sparkles: Async Axios in Asynchronous Javascript and Promises

---
+++ 17-Javascript Assíncrono e Promises/10-AsyncAxios.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+async function fetchRepos() {
+
+    try {
+        const user = await axios.get('https://api.github.com/users/maykbrito');
+        const repos = await axios.get(user.data.repos_url)
+        console.log(repos.data)
+    } catch(e) {
+        console.log(e)
+    }
+
+}
+
+fetchRepos()
