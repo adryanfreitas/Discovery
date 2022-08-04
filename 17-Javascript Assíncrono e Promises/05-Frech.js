abc7287e0f83eb5cb236825c77abbee2e98f7c48
Adryan da Luz Freitas <Adryan.luz.freitas@gmail.com>
2022-08-04 10:16:45 -0400

    feat: :sparkles: Frech in Asynchronous Javascript and Promises

---
+++ 17-Javascript Assíncrono e Promises/05-Frech.js
@@ -0,0 +1,6 @@
+fetch('https://api.github.com/users/maykbrito')
+.then( Response => Response.json())
+.then( data => fetch(data.repos_url))
+.then( res => res.json)
+.then( d => console.log(d))
+.catch( err => console.log(err))
